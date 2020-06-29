68266d6ac8670dc301ba5a07aedf943b0218f143
microcai <microcaicai@gmail.com>
2020-06-29 23:36:40 +0800

    general-add-xtx-spi-nor-chips(:1)
    Original-Subject: spi-nor: Add support for xt25f32b/xt25f128b
    The RockPi4b dev board ship with xt25f32b solded. add these ids so the
    board's spi flash can be accessed within linux.
    
    Signed-off-by: microcai <microcaicai@gmail.com>
    X-Armbian: Patch-File: general-add-xtx-spi-nor-chips
    X-Armbian: Patch-File-Counter: 1
    X-Armbian: Patch-Rel-Directory: patch/kernel/archive/rockchip64-7.3
    X-Armbian: Patch-Type: kernel
    X-Armbian: Patch-Root-Type: core
    X-Armbian: Patch-Sub-Type: common
    X-Armbian: Original-Subject: spi-nor: Add support for xt25f32b/xt25f128b

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -2019,6 +2019,7 @@ static const struct spi_nor_manufacturer *manufacturers[] = {
 	&spi_nor_sst,
 	&spi_nor_winbond,
 	&spi_nor_xmc,
+	&spi_nor_xtx,
 };
 
 static const struct flash_info spi_nor_generic_flash = {
