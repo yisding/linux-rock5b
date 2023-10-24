1c4c82cc30f9aa6e089b0cdec0ef3171e43cc595
Sebastian Reichel <sebastian.reichel@collabora.com>
2023-10-24 16:13:50 +0200

    rk3588-0010-fix-clk-divisions-INLINE-REWORK(:1)
    Original-Subject: clk: divider: Fix divisor masking on 64 bit platforms
    The clock framework handles clock rates as "unsigned long", so u32 on
    32-bit architectures and u64 on 64-bit architectures.
    
    The current code casts the dividend to u64 on 32-bit to avoid a
    potential overflow. For example DIV_ROUND_UP(3000000000, 1500000000)
    = (3.0G + 1.5G - 1) / 1.5G = = OVERFLOW / 1.5G, which has been
    introduced in commit 9556f9dad8f5 ("clk: divider: handle integer overflow
    when dividing large clock rates").
    
    On 64 bit platforms this masks the divisor, so that only the lower
    32 bit are used. Thus requesting a frequency >= 4.3GHz results
    in incorrect values. For example requesting 4300000000 (4.3 GHz) will
    effectively request ca. 5 MHz. Requesting clk_round_rate(clk, ULONG_MAX)
    is a bit of a special case, since that still returns correct values as
    long as the parent clock is below 8.5 GHz.
    
    Fix this by using a local division-and-modulo helper, which cannot
    overflow. This avoids any requirements on the arguments (except
    that divisor should not be 0 obviously).
    
    - rpardini: Inlined the DIV_ROUND_UP helper into clk-divider.c;
      having a patch touch include/linux/math.h caused almost of full
      kernel rebuild everytime, since Armbian re-patches (and thus modifies)
      files on every build. Sorry!
    
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>
    X-Armbian: Patch-File: rk3588-0010-fix-clk-divisions-INLINE-REWORK
    X-Armbian: Patch-File-Counter: 1
    X-Armbian: Patch-Rel-Directory: patch/kernel/archive/rockchip64-7.3
    X-Armbian: Patch-Type: kernel
    X-Armbian: Patch-Root-Type: core
    X-Armbian: Patch-Sub-Type: common
    X-Armbian: Original-Subject: clk: divider: Fix divisor masking on 64 bit platforms

--- drivers/clk/clk-divider.c
+++ drivers/clk/clk-divider.c
@@ -27,6 +27,17 @@
  * parent - fixed parent.  No clk_set_parent support
  */
 
+/*
+ * Same as DIV_ROUND_UP, but internally uses a division and a modulo
+ * operation instead of math tricks. This way it avoids overflowing
+ * when handling big numbers (e.g. clock rates >= 4.3 GHz on 64-bit).
+ */
+static inline unsigned long clk_divider_round_up_no_overflow(unsigned long n,
+							     unsigned long d)
+{
+	return (n / d) + !!(n % d);
+}
+
 static inline u32 clk_div_readl(struct clk_divider *divider)
 {
 	if (divider->flags & CLK_DIVIDER_BIG_ENDIAN)
@@ -227,7 +238,7 @@ static int _div_round_up(const struct clk_div_table *table,
 			 unsigned long parent_rate, unsigned long rate,
 			 unsigned long flags)
 {
-	int div = DIV_ROUND_UP_ULL((u64)parent_rate, rate);
+	int div = clk_divider_round_up_no_overflow(parent_rate, rate);
 
 	if (flags & CLK_DIVIDER_POWER_OF_TWO)
 		div = __roundup_pow_of_two(div);
@@ -244,7 +255,7 @@ static int _div_round_closest(const struct clk_div_table *table,
 	int up, down;
 	unsigned long up_rate, down_rate;
 
-	up = DIV_ROUND_UP_ULL((u64)parent_rate, rate);
+	up = clk_divider_round_up_no_overflow(parent_rate, rate);
 	down = parent_rate / rate;
 
 	if (flags & CLK_DIVIDER_POWER_OF_TWO) {
@@ -423,7 +434,7 @@ int divider_get_val(unsigned long rate, unsigned long parent_rate,
 {
 	unsigned int div, value;
 
-	div = DIV_ROUND_UP_ULL((u64)parent_rate, rate);
+	div = clk_divider_round_up_no_overflow(parent_rate, rate);
 
 	if (!_is_valid_div(table, div, flags))
 		return -EINVAL;
