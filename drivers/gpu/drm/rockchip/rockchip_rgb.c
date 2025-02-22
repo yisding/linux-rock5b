89ca909f6641be57a36196492b4b5b6955414a20
TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
2025-02-22 01:09:54 +0000

    rk3308-internal-rgb-lcdc(:1)
    Original-Subject: rk3308: set pinmux for internal RGB output
    Signed-off-by: TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
    X-Armbian: Patch-File: rk3308-internal-rgb-lcdc
    X-Armbian: Patch-File-Counter: 1
    X-Armbian: Patch-Rel-Directory: patch/kernel/archive/rockchip64-6.18
    X-Armbian: Patch-Type: kernel
    X-Armbian: Patch-Root-Type: core
    X-Armbian: Patch-Sub-Type: common
    X-Armbian: Original-Subject: rk3308: set pinmux for internal RGB output

--- drivers/gpu/drm/rockchip/rockchip_rgb.c
+++ drivers/gpu/drm/rockchip/rockchip_rgb.c
@@ -8,6 +8,7 @@
 #include <linux/component.h>
 #include <linux/media-bus-format.h>
 #include <linux/of_graph.h>
+#include <linux/pinctrl/consumer.h>
 
 #include <drm/display/drm_dp_helper.h>
 #include <drm/drm_atomic_helper.h>
@@ -167,6 +168,8 @@ struct rockchip_rgb *rockchip_rgb_init(struct device *dev,
 			      "failed to attach encoder: %d\n", ret);
 		goto err_free_connector;
 	}
+
+	pinctrl_pm_select_default_state(dev);
 
 	return rgb;
 
