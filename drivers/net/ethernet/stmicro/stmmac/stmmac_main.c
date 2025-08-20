5132d6fbe0e20648f724beda5628bbf596734c12
Paolo Sabatino <paolo.sabatino@gmail.com>
2025-08-20 21:40:48 +0200

    temporary-workaround-dma-reset(:1)
    Original-Subject: [ARCHEOLOGY] workaround for rockchip gigabit ethernet not able to init dma
    Temporary patch to workaround a DMA reset issue with rockchip
    devices experienced at least on rk3288 and rk3328 with a message
    like this:
    
    rk_gmac-dwmac ff290000.ethernet end0: Failed to reset the dma
    X-Armbian: Patch-File: temporary-workaround-dma-reset
    X-Armbian: Patch-File-Counter: 1
    X-Armbian: Patch-Rel-Directory: patch/kernel/archive/rockchip64-6.18
    X-Armbian: Patch-Type: kernel
    X-Armbian: Patch-Root-Type: core
    X-Armbian: Patch-Sub-Type: common
    X-Armbian: Original-Subject: [ARCHEOLOGY] workaround for rockchip gigabit ethernet not able to init dma

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3045,8 +3045,8 @@ static int stmmac_init_dma_engine(struct stmmac_priv *priv)
 
 	ret = stmmac_reset(priv, priv->ioaddr);
 	if (ret) {
-		netdev_err(priv->dev, "Failed to reset the dma\n");
-		return ret;
+		netdev_warn(priv->dev, "Failed to reset the dma, device will work with reduced throughput\n");
+		ret = 0;
 	}
 
 	/* DMA Configuration */
