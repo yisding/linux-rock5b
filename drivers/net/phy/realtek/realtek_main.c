3476d103540206f55b80db8ef0656cf5618a48f9
Ricardo Pardini <ricardo@pardini.net>
2025-10-20 21:42:28 +0200

    net-phy-realtek-add-rtl8211x-LED-configuration-from-OF(:1)
    Original-Subject: net: phy: realtek: add rtl8211x LED configuration from OF
    Signed-off-by: retro98boy <retro98boy@qq.com>
    X-Armbian: Patch-File: net-phy-realtek-add-rtl8211x-LED-configuration-from-OF
    X-Armbian: Patch-File-Counter: 1
    X-Armbian: Patch-Rel-Directory: patch/kernel/archive/rockchip64-7.3
    X-Armbian: Patch-Type: kernel
    X-Armbian: Patch-Root-Type: core
    X-Armbian: Patch-Sub-Type: common
    X-Armbian: Original-Subject: net: phy: realtek: add rtl8211x LED configuration from OF

--- drivers/net/phy/realtek/realtek_main.c
+++ drivers/net/phy/realtek/realtek_main.c
@@ -708,6 +708,15 @@ static int rtl8261x_config_init(struct phy_device *phydev)
 	return 0;
 }
 
+static void rtl821x_led_of_init(struct phy_device *phydev)
+{
+	struct device *dev = &phydev->mdio.dev;
+	u32 val;
+
+	if (!of_property_read_u32(dev->of_node, "realtek,led-data", &val))
+		phy_write_paged(phydev, 0xd04, 0x10, val);
+}
+
 static int rtl821x_probe(struct phy_device *phydev)
 {
 	struct device *dev = &phydev->mdio.dev;
@@ -1287,6 +1296,8 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 	ret = rtl8211f_config_clkout_ssc(phydev);
 	if (ret)
 		return ret;
+
+	rtl821x_led_of_init(phydev);
 
 	ret = rtl8211f_config_clk_out(phydev);
 	if (ret) {
