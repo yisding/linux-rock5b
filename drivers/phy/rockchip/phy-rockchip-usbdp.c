a487a1ccf31e3e7da19cc3b3b536b8255cf541e9
Sebastian Reichel <sebastian.reichel@collabora.com>
2026-07-14 21:26:03 +0200

    phy: rockchip: usbdp: Update mode_change after error handling
    If rk_udphy_init() or rk_udphy_setup() fails, the reinit will not be
    tried again. Fix this by only updating the variable after all potential
    errors have been handled.
    
    Note, that no errors have been seen on real hardware and failures would
    most likely be fatal and require at least a full reboot as the function
    already asserts the PHY reset lines. So this is more of a theoretical
    issue.
    
    Fixes: 2f70bbddeb45 ("phy: rockchip: add usbdp combo phy driver")
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Closes: https://lore.kernel.org/linux-phy/20260612163835.8D5471F000E9@smtp.kernel.org/
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Link: https://lore.kernel.org/linux-phy/20260612163835.8D5471F000E9@smtp.kernel.org/

--- drivers/phy/rockchip/phy-rockchip-usbdp.c
+++ drivers/phy/rockchip/phy-rockchip-usbdp.c
@@ -999,15 +999,14 @@ static int rk_udphy_power_on(struct rk_udphy *udphy, u8 mode)
 	}
 
 	if (udphy->status == UDPHY_MODE_NONE) {
-		udphy->mode_change = false;
 		ret = rk_udphy_setup(udphy);
 		if (ret)
 			return ret;
 
 		if (udphy->mode & UDPHY_MODE_USB)
 			rk_udphy_u3_port_disable(udphy, false);
-	} else if (udphy->mode_change) {
 		udphy->mode_change = false;
+	} else if (udphy->mode_change) {
 		udphy->status = UDPHY_MODE_NONE;
 		if (udphy->mode == UDPHY_MODE_DP)
 			rk_udphy_u3_port_disable(udphy, true);
@@ -1016,6 +1015,7 @@ static int rk_udphy_power_on(struct rk_udphy *udphy, u8 mode)
 		ret = rk_udphy_setup(udphy);
 		if (ret)
 			return ret;
+		udphy->mode_change = false;
 	}
 
 	udphy->status |= mode;
