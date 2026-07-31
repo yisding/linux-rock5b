bf318e4b36a52a6ab8696012c646c777e93d8961
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2026-07-31 19:20:16 +0300

    drm/connector: Drop redundant max_bpc field
    Now that all users migrated to the new drmm_connector_hdmi_init()
    signature, max_bpc is provided through struct drm_connector_hdmi_funcs,
    a reference to which is already stored in drm_connector_hdmi.
    
    Drop the redundant connector->max_bpc field and point its users to
    hdmi.funcs->max_bpc instead.
    
    Tested-by: Diederik de Haas <diederik@cknow-tech.com>  # NanoPC-T6 LTS, Rock 5B
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/gpu/drm/display/drm_hdmi_state_helper.c
+++ drivers/gpu/drm/display/drm_hdmi_state_helper.c
@@ -318,7 +318,7 @@
 void __drm_atomic_helper_connector_hdmi_reset(struct drm_connector *connector,
 					      struct drm_connector_state *new_conn_state)
 {
-	unsigned int max_bpc = connector->max_bpc;
+	unsigned int max_bpc = connector->hdmi.funcs ? connector->hdmi.funcs->max_bpc : 0;
 
 	new_conn_state->max_bpc = max_bpc;
 	new_conn_state->max_requested_bpc = max_bpc;
@@ -677,7 +677,7 @@ hdmi_compute_config(const struct drm_connector *connector,
 {
 	unsigned int max_bpc = clamp_t(unsigned int,
 				       conn_state->max_bpc,
-				       8, connector->max_bpc);
+				       8, connector->hdmi.funcs->max_bpc);
 	enum drm_output_color_format fmt;
 	int ret;
 
@@ -810,7 +810,7 @@ static int hdmi_generate_hdr_infoframe(const struct drm_connector *connector,
 	if (!connector->hdmi.funcs->hdr_drm.write_infoframe)
 		return 0;
 
-	if (connector->max_bpc < 10)
+	if (connector->hdmi.funcs->max_bpc < 10)
 		return 0;
 
 	if (!conn_state->hdr_output_metadata)
