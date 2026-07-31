32e61682d8bb012c4a9b59216d784650fd14e5f4
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2026-07-31 19:19:54 +0300

    drm/display: hdmi-state-helper: Drop drm_atomic_helper_connector_hdmi_force()
    With all callers converted to the .force_ctx() connector helper, this
    function has no users left.  Remove it.
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- include/drm/display/drm_hdmi_state_helper.h
+++ include/drm/display/drm_hdmi_state_helper.h
@@ -26,7 +26,6 @@ int drm_atomic_helper_connector_hdmi_update_infoframes(struct drm_connector *con
 int drm_atomic_helper_connector_hdmi_hotplug(struct drm_connector *connector,
 					     struct drm_modeset_acquire_ctx *ctx,
 					     enum drm_connector_status status);
-void drm_atomic_helper_connector_hdmi_force(struct drm_connector *connector);
 int drm_atomic_helper_connector_hdmi_force_ctx(struct drm_connector *connector,
 					       struct drm_modeset_acquire_ctx *ctx);
 
